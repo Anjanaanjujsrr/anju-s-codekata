0cac5a74e3f84a374a1b446b242839c4c0588068
Anjanaanjujsrr <50616298+Anjanaanjujsrr@users.noreply.github.com>
2019-05-16 09:20:11 +0530

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -1,12 +1,5 @@
-#include<stdio.h>
-int main()
-{
-char c;
-printf("enter the character");
-scanf("%c",&c);
-if((c>'a'&&c<'z')||(c>'A'&&c<'Z'))
-printf("%c is alphabet",c);
-else
-printf("%c is nat a alphabet",c);
-return 0;
-}
+c=input()
+if((c>='a'and c<='z')or(c>='A' and c<='Z')):
+    print("Alphabet")
+else:
+    print("No")
