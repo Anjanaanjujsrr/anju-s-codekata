80125850e86c235a9300a60220540fec0e8aaf48
Anjanaanjujsrr <50616298+Anjanaanjujsrr@users.noreply.github.com>
2019-05-16 12:03:58 +0530

    Add files via upload

---
+++ ams bet.py
@@ -0,0 +1,13 @@
+l,u=input().split()
+l=int(l)
+u=int(u)
+for n in range(l, u+ 1):
+    order = len(str(n))
+    s = 0
+    temp = n
+    while temp > 0:
+        digit = temp % 10
+        s += digit ** order
+        temp //= 10
+    if n == s:
+       print(n)
