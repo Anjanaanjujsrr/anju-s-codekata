16c510eb27e0297eabe3c5f69ea5506dc94c87c4
Anjanaanjujsrr <50616298+Anjanaanjujsrr@users.noreply.github.com>
2019-05-19 13:12:41 +0530

    Update set 5i.py

--- set 5i.py
+++ set 5i.py
@@ -1,5 +1,12 @@
-a = int(input())
-if not(a & 1):
-    print ("yes")
-else:
-    print ("no")
+X=int(input())
+K=X
+S=0
+while X>1:
+    if X%2==0:
+        X=X/2
+        S=S+1
+    else:
+        print("no")
+        break
+if (2**S)==K:
+    print("yes")
