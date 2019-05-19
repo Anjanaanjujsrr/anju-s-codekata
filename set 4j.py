fd7f7df7a9e702606fe0ece3eb3a3e5bc4292a1e
Anjanaanjujsrr <50616298+Anjanaanjujsrr@users.noreply.github.com>
2019-05-19 12:13:37 +0530

    Update set 4j.py

--- set 4j.py
+++ set 4j.py
@@ -1,6 +1,17 @@
-n=int(input())
-x,y=0,1
-
-while y<=n:
-    print(y)
-    x,y = y,x+y
+N=input()
+N=int(N)
+A=1
+B=1
+if(N==1):
+    print(A,end='')
+count=0
+if(N>1):
+    while(count<N):
+        if(count==N-1):
+            print(A,end='')
+        else:
+            print(A,end=' ')
+        Nth=A+B
+        A=B
+        B=Nth
+        count=count+1
